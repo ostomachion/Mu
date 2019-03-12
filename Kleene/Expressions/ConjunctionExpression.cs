fa9b5d1502f6174111da6e05f578c8bb90ebcfc0
Josh Hufford <josh@hufford.io>
2019-03-12 16:46:52 -0400

    Add skeleton of ConjunctionExpression.cs.

---
+++ Kleene/Expressions/ConjunctionExpression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kleene
+{
+    public class ConjunctionExpression : VariadicExpression
+    {
+        public ConjunctionExpression(IEnumerable<Expression> expressions)
+            : base(expressions) { }
+
+        public override Result Generate() => throw new NotImplementedException();
+    }
+}
